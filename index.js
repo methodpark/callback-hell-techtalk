45a0f570a31208a6b705fe3512f78649cf3be6bc
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2016-08-15 15:46:47 +0200

    Refactor example to use promises
    We still have problems with missing closures. But the workflow is much better visible now. But there's a small behavioural difference now:
    
    If one of the first two socials fails, the following socials are no longer executed!

--- index.js
+++ index.js
@@ -2,61 +2,87 @@ const web = require('./support/web'),
       social = require('./support/social'),
       fs = require('fs');
 
-function publishFile(path, callback) {
-    fs.readFile(path, (error, fileContent) => {
-        if (error) {
-            callback(error);
-            return;
-        }
 
-        web.get('https://login.example.com/', (response) => {
-            if (response.status != 200) {
-                callback('Could not login: ' + response.status);
+let readFileAsync = path => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (error, fileContent) => {
+            if (error) {
+                reject(error);
                 return;
             }
 
-            web.post('https://submit.example.com/', fileContent, (response) => {
-                let done = {
-                        twitter: false,
-                        facebook: false,
-                        plus: false
-                    },
-                    errors = [],
-                    handleDone = (what, error, response) => {
-                        done[what] = response || true;
-
-                        if (error) {
-                            errors.push(`${what}: ${error}`);
-                        }
-
-                        if (done.twitter && done.facebook && done.plus) {
-                            if (errors.length === 0) {
-                                errors = undefined;
-                            }
-                            callback(errors);
-                        }
-                    };
-
-                social.tweet('Just published this thing: ' + response.url, (error, response) => {
-                    handleDone('twitter', error, response);
-                });
-
-                social.facebook('Just published this thing: ' + response.url, (error, response) => {
-                    handleDone('facebook', error, response);
-                });
-
-                social.plus('Just published this thing: ' + response.url, (error, response) => {
-                    handleDone('plus', error, response);
-                });
-            });
+            resolve(fileContent);
         });
     });
+};
+
+let loginAsync = (url) => {
+    return new Promise((resolve, reject) => {
+        web.get(url, response => {
+            if (response.status !== 200) {
+                reject(`Could not login: ${response.status}`);
+                return;
+            }
+
+            resolve();
+        });
+    });
+};
+
+let postFileAsync = (url, content) => {
+    return new Promise((resolve, reject) => {
+        web.post(url, content, response => {
+            if (response.status !== 200) {
+                reject(`Could not submit: ${response.status}`);
+                return;
+            }
+
+            resolve(response.url);
+        });
+    });
+};
+
+let makeSocialAsync = (what) => {
+    return (url) => {
+        return new Promise((resolve, reject) => { 
+            social[what](url, (error, response) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(response);
+            });
+        });
+    };
 }
 
-publishFile('./support/web.js', (error) => {
-    if (error) {
-        console.warn(error);
-    }
+let socialAsync = {
+    tweet: makeSocialAsync('tweet'),
+    facebook: makeSocialAsync('facebook'),
+    plus: makeSocialAsync('plus')
+};
 
-    console.log('done');
-});
+let fileContent;
+let uploadUrl;
+readFileAsync('./support/web.js')
+    .then(content => {
+        fileContent = content;
+        return loginAsync('https://login.example.com/');
+    })
+    .then(result => {
+        return postFileAsync('https://submit.example.com/', fileContent);
+    })
+    .then(() => {
+        return socialAsync.tweet(uploadUrl);
+    })
+    .then(url => {
+        uploadUrl = url;
+        return socialAsync.facebook(uploadUrl);
+    })
+    .then(() => {
+        return socialAsync.plus(uploadUrl);
+    })
+    .catch(error => {
+        console.warn(error);
+    });
